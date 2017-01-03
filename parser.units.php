f06e6d7d31d498e9dc8782dfccc3614c53d5b5ba
Faat <i@faat.ru>
2017-01-03 21:28:19 +0300

    Add coefficients parser

--- parser.units.php
+++ parser.units.php
@@ -4,17 +4,34 @@ $mil = file_get_contents('military/mil.txt');
 
 preg_match_all('#technology = \{[^\}]+\}#i', $mil, $techs);
 
-$result = array();
+$result = [];
+$groups = [];
 
-$groups = array();
+$coefficients = [
+    'infantry_fire' => [],
+    'infantry_shock' => [],
+    'cavalry_fire' => [],
+    'cavalry_shock' => [],
+    'land_morale' => [],
+    'military_tactics' => ["0" => 0.5],
+];
+
 foreach ($techs[0] as $i => $tech) {
     preg_match_all('#enable = ([^\n]+)\n#i', $tech, $units);
 
+    foreach ($coefficients as $key => &$arr) {
+        preg_match('#' . $key . '\s+=\s+([^\n\#]+)\n#i', $tech, $coeff);
+
+        if (isset($coeff[1]) && $coeff = trim($coeff[1])) {
+            $arr[$i] = (float)$coeff;
+        }
+    }
+    unset($arr);
+
     foreach ($units[1] as $u) {
+        $un = file('military/units/' . trim($u) . '.txt');
 
-        $un = file('military/units/'.trim($u).'.txt');
-
-        $unit = array();
+        $unit = [];
         foreach ($un as $uni) {
             $unity = explode(' = ', $uni);
             if (isset($unity[1])) {
@@ -37,4 +54,10 @@ foreach ($techs[0] as $i => $tech) {
     }
 }
 
+$coefficientsText = '';
+foreach ($coefficients as $key => $arr) {
+    $coefficientsText .= 'var ' . $key .' = ' . json_encode($arr, JSON_PRETTY_PRINT) . ';' . PHP_EOL;
+}
+
 file_put_contents('units.js', "var units = ".json_encode($result, JSON_PRETTY_PRINT).";");
+file_put_contents('coefficients.js', $coefficientsText);
